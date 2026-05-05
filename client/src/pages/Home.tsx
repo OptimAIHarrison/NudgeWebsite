e0d7970b61f183bfc7cf0c267d047732accf7f15
Harrison <harrison@nudgedigital.com.au>
2026-05-05 05:14:24 -0400

    Design: Move notification panels out more, reduce main text size for better hero composition

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -21,12 +21,12 @@ function ServiceNotification({
   size?: 'sm' | 'md';
 }) {
   const positionClasses = {
-    'tl': 'top-1/4 left-2/5 -translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
-    'tr': 'top-1/4 right-2/5 translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
-    'bl': 'top-3/4 left-2/5 -translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
-    'br': 'top-3/4 right-2/5 translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
-    'cl': 'top-1/2 left-1/3 -translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
-    'cr': 'top-1/2 right-1/3 translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
+    'tl': 'top-1/4 left-1/3 -translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
+    'tr': 'top-1/4 right-1/3 translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
+    'bl': 'top-3/4 left-1/3 -translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
+    'br': 'top-3/4 right-1/3 translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
+    'cl': 'top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
+    'cr': 'top-1/2 right-1/4 translate-x-1/2 -translate-y-1/2 hidden lg:block z-20',
   };
 
   const sizeClasses = {
